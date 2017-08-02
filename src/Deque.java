3d9666df0709be67276307b039a96fb406f1224e
Greg Dardis <dardis.greg@gmail.com>
2017-08-02 12:26:17 -0600

    added inner Node class for linked list implementation of deque. throw exceptions if trying to add a null item

--- src/Deque.java
+++ src/Deque.java
@@ -1,30 +1,54 @@
 
 public class Deque<Item> implements Iterable<Item> {
+    
+    private int stackSize;
+    
+    /* Top of the stack */
+    private Node first;
+    
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+    
     public Deque() {
         // construct an empty deque
     }
+ 
     public boolean isEmpty() {
         // is the deque empty?
     }
+    
     public int size() {
         // return the number of items on the deque
     }
+    
     public void addFirst(Item item) {
-        // add the item to the front
+        if (item == null) {
+            throw new IllegalArgumentException("Cannot add null item to deque.");
+        }
     }
+    
     public void addLast(Item item) {
-        // add the item to the end
+        if (item == null) {
+            throw new IllegalArgumentException("Cannot add null item to deque.");
+        }
     }
+    
     public Item removeFirst() {
         // remove and return the item from the front
     }
+    
     public Item removeLast()  {
         // remove and return the item from the end
     }
+    
     public Iterator<Item> iterator() {
         // return an iterator over items in order from front to end
     }
+    
     public static void main(String[] args) {
         // unit testing (optional)
     }
+    
 }
