aeab7f60006cafcd9b4662e9415ca67756953d1a
Greg Dardis <dardis.greg@gmail.com>
2017-08-03 16:09:05 -0600

    implemented resize, enqueue, size, constructor, isempty to RandomizedQueue

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,27 +1,62 @@
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-//    public RandomizedQueue() {
-//        // construct an empty randomized queue
-//    }
-//    public boolean isEmpty() {
-//        // is the queue empty?
-//    }
-//    public int size() {
-//        // return the number of items on the queue
-//    }
-//    public void enqueue(Item item) {
-//        // add the item
-//    }
-//    public Item dequeue() {
-//        // remove and return a random item
-//    }
-//    public Item sample() {
-//        // return (but do not remove) a random item
-//    }
-//    public Iterator<Item> iterator() {
-        // return an independent iterator over items in random order
-//    }
-//    public static void main(String[] args) {
-//        // unit testing (optional)
-//    }
+    
+    private Item[] items;
+    private int queueSize;
+    
+    public RandomizedQueue() {
+        items = (Item[]) new Object[1];
+        queueSize = 0;
+    }
+    
+    public boolean isEmpty() {
+        return queueSize == 0;
+    }
+    
+    public int size() {
+        return queueSize;
+    }
+    
+    /**
+     * Adds an item to the end of the queue array.
+     * 
+     * @param item  Item to add
+     */
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Cannot add null item to randomized queue.");
+        }
+        items[queueSize++] = item;
+        if (queueSize == items.length) {
+            resize();
+        }
+    }
+    
+    /**
+     * Doubles the size of the items array.
+     */
+    private void resize() {
+        Item[] temp = (Item[]) new Object[queueSize * 2];
+        for (int i = 0; i < queueSize; i++) {
+            temp[i] = items[i];
+        }
+        items = temp;
+    }
+    
+    public Item dequeue() {
+        // remove and return a random item
+    }
+    
+    public Item sample() {
+        // return (but do not remove) a random item
+    }
+    
+    public Iterator<Item> iterator() {
+         return an independent iterator over items in random order
+    }
+    
+    public static void main(String[] args) {
+        // unit testing (optional)
+    }
+    
 }
